adfb0aa3cf225ec14532d26bc01746105e9a2344
Javier Banuelos <J.Banuelos00@gmail.com>
2021-01-14 00:35:01 -0800

    Get and Post
    Created app.get() and app.post() lines in calculator.js so we can get both the root directory and post something when we submit.

--- calculator.js
+++ calculator.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', (req, res) => {
+  res.send("Thanks for posting!");
+})
 
 app.listen(port, () => {
   console.log(`Server has started at http://localhost:${port}`);
