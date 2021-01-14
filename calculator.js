3994784d880502edfd3fc249df43a6c099ed4ab7
Javier Banuelos <J.Banuelos00@gmail.com>
2021-01-14 01:07:05 -0800

    BMI Calculator
    Added in an html file containing a form requesting inputs for a BMI calculator. In the calculator.js file, added in the appropriate Get and Post code to receive the input from /bmicalculator and calculate the correct BMI based on the simple formula found on the internet.

--- calculator.js
+++ calculator.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+})
+
 app.post('/', (req, res) => {
   let num1 = Number(req.body.num1);
   let num2 = Number(req.body.num2);
@@ -17,6 +21,15 @@ app.post('/', (req, res) => {
   let result = num1 + num2;
 
   res.send("The sum is " + result);
+})
+
+app.post('/bmicalculator', (req, res) => {
+  let weight = Number(req.body.weight);
+  let height = Number(req.body.height);
+
+  let bmi = 703*(weight/(height * height));
+
+  res.send("Your BMI is " + bmi)
 })
 
 app.listen(port, () => {
