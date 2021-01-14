92b7a88da66c0690fea25030063604edb25d1b11
Javier Banuelos <J.Banuelos00@gmail.com>
2021-01-14 00:48:18 -0800

    Maths from body-parser
    Worked in calculator.js to require body-parser, then use that to grab the values from num1 input and num2 input. We converted those values to numbers and added them in the app.post().

--- calculator.js
+++ calculator.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded( {extended: true} ));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', (req, res) => {
-  res.send("Thanks for posting!");
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  let result = num1 + num2;
+
+  res.send("The sum is " + result);
 })
 
 app.listen(port, () => {
